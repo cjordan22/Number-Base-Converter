97e1b56cb957255d813a5b9a5f4138a83fe428e9
Chad Jordan <cjordan22@student.gsu.edu>
2013-10-10 02:28:02 -0400

    Added some code but it is wrong
    Check the comments for details on how to do (should work) tomorrow.
    Basically I've been trying to return a function. I should return one int
    at a time and store them in a new int array

--- NumberBaseConverter.cpp
+++ NumberBaseConverter.cpp
@@ -1,14 +1,51 @@
 #include <iostream>
+using namespace std;
+char letterconverter(char* i);
+
 int main()
 {
-  int number;
+  char number[100] = "";
+  char actual[100] = "";
   int base1;
   int base2;
-  cout << "Enter the number you wish to have converted.";
+  cout << "Enter the number you wish to have converted (up to 10 digits).\n";
   cin >> number;
-  cout << "What base is it?";
+  cout << "What base is it?\n";
   cin >> base1;
-  cout << "What base do you want it to be?";
+  cout << "What base do you want it to be?\n";
   cin>>base2;
-  cout << "This is a test. Your number is: " << number <<". It is base " << base1 << ". You want it to be base " <<base2; //DEBUG/TEST
+  //cout << "This is a test. Your number is: " << number <<". The length is: "<<strlen(number)<<". It is base " << base1 << ". You want it to be base " <<base2 <<"\n"; //DEBUG/TEST
+  //*actual = letterconverter(number);				//commented out
+  /*
+  for (int i =0;actual[i];i++)
+  {
+	  cout << actual[i];
+  }*/						//Broken atm, YOU CANT RETURN ARRAYS IN C++ should increment through and add to array in here while printing. increment through number. if it is a digit, print, convert to int and add to actual (MAKE INT ARRAY), if not get a return int from convert
+
+}
+
+char letterconverter(char* number)
+{
+	char actual[100]="";
+	int i = 0;
+	for(int k = 0; number[k]; k++)
+	{
+	  if (isdigit(number[k]))
+	  { 
+		  actual[i]=number[k];
+		  i++;
+	  }
+	  else 
+	  {
+			switch (number[k])
+			{
+			case 'a': actual[i]='1'; actual[i+1]='0'; i = i+2; break;
+			}
+	  }
+	  for (int i =0;actual[i];i++)
+  {
+	  cout << actual[i];
+  }
+	}
+	return *actual;
 }
