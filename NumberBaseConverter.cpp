7c78526a13bd67ca4a3df929e8d9481afdd0e7d8
cjordan22 <cjordan22@student.gsu.edu>
2013-10-10 00:45:21 -0400

    Added the basic beginning cin/cout commands

--- NumberBaseConverter.cpp
+++ NumberBaseConverter.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 int main()
 {
-
+  int number;
+  int base1;
+  int base2;
+  cout << "Enter the number you wish to have converted.";
+  cin >> number;
+  cout << "What base is it?";
+  cin >> base1;
+  cout << "What base do you want it to be?";
+  cin>>base2;
+  cout << "This is a test. Your number is: " << number <<". It is base " << base1 << ". You want it to be base " <<base2; //DEBUG/TEST
 }
